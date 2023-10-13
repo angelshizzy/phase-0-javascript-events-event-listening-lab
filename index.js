a98a8e6976b22bd160f6d431dae97790793a5b35
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-14 00:21:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    let input = document.getElementById("input")
+        input.addEventListener("click", clickAlert)
+
+        function clickAlert (){
+            alert('I was clicked')
+        }
 }
+
+addingEventListener();
